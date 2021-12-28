8017d7770bf7178cd3cf064a17fa3c36b9b1d5c3
Diesel <jpedro.diesel@gmail.com>
2021-12-28 20:32:50 -0300

    pixels art project

--- script.js
+++ script.js
@@ -2,47 +2,53 @@
 
 const clearButton = document.getElementById('clear-board');
 const colors = document.querySelectorAll('.color');
- 
-function changeSelectedColor (event) {
+
+function changeSelectedColor(event) {
   const selected = document.querySelector('.selected');
-    if (selected != null) {
-      selected.classList.remove('selected');
-      event.target.classList.add('selected');
-    }
+  if (selected != null) {
+    selected.classList.remove('selected');
     event.target.classList.add('selected');
-  } 
-function selectColor () {
-  for (let index = 0; index < colors.length; index += 1){
-  colors[index].addEventListener('click', changeSelectedColor); 
- }
+  }
+  event.target.classList.add('selected');
+}
+function selectColor() {
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].addEventListener('click', changeSelectedColor);
+  }
 }
 
-// Requisito 8 
+// Requisito 8
 const pixels = document.querySelectorAll('.pixel');
 
-function paintPixel (event) {
+function paintPixel(event) {
   const selectedColor = document.querySelector('.selected');
-  const colorToApply = window.getComputedStyle(selectedColor).getPropertyValue('background-color')
-  event.target.style.backgroundColor = colorToApply
+  const colorToApply = window.getComputedStyle(selectedColor).getPropertyValue('background-color');
+  const clickedSquare = event.target;
+  clickedSquare.style.backgroundColor = colorToApply;
 }
 
-// pixels.addEventListener('click', paintPixel); 
-
-function pixelsToPaint () {
-  for (let index = 0; index < pixels.length; index += 1){
-  pixels[index].addEventListener('click', paintPixel); 
- }
+function pixelsToPaint() {
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', paintPixel);
+  }
 }
 
-pixelsToPaint(colors)
+// Requisito 7 e 8 foram feitos com ajuda no Slack da turma e os seguintes site:
+// https://stackoverflow.com/questions/13980101/get-background-color-of-a-css-class-applied-to-an-element
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_getcomputedstyle
 
 // Requisito 9
-function clearBoard () {
-  const pixelToClear = document.querySelectorAll('.pixel') 
+function clearBoard() {
+  const pixelToClear = document.querySelectorAll('.pixel');
   for (let index = 0; index < pixelToClear.length; index += 1) {
-  pixelToClear[index].style.backgroundColor = 'white';
- }
+    pixelToClear[index].style.backgroundColor = 'white';
+  }
 }
 
+// Feito com a ajuda dos seguintes links:
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
+// https://stackoverflow.com/questions/195951/how-can-i-change-an-elements-class-with-javascript
+
 clearButton.addEventListener('click', clearBoard);
-selectColor(colors);
+pixelsToPaint();
+selectColor();
